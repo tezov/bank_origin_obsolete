8ff4a5f85f68dad02e9a5c27b76bc4066b86b43a
Prive <tezov.app@gmail.com>
2023-03-19 12:48:08 +0100

    wip - multi state
    Signed-off-by: Prive <tezov.app@gmail.com>

--- lib_core_android_kotlin/src/main/kotlin/com/tezov/lib_core_android_kotlin/ui/theme/theme/ThemeTypographiesExtended.kt
+++ lib_core_android_kotlin/src/main/kotlin/com/tezov/lib_core_android_kotlin/ui/theme/theme/ThemeTypographiesExtended.kt
@@ -2,7 +2,7 @@
  *  *********************************************************************************
  *  Created by Tezov on 19/03/2023 12:48
  *  Copyright (c) 2023 . All rights reserved.
- *  Last modified 19/03/2023 12:47
+ *  Last modified 19/03/2023 12:48
  *  First project bank / bank.lib_core_android_kotlin.main
  *  This file is private and it is not allowed to use it, copy it or modified it
  *  without the permission granted by the owner Tezov. For any request request,
